47f2d012cf9c29b9caab875b0165befaf1e4f1e8
Javier Salcedo <javiersalcedo75@gmail.com>
2016-09-10 23:07:59 -0300

    probando ahora message

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ const client = new Wit({accessToken: 'W7FRSECWMUW4MOC45NYCCWORC2ZIBLIE'});
 
 
 function sendToBot(userID,messageText){
-  client.runActions(userID, messageText, {})
+  client.message(userID, messageText, {})
   .then((data) => {
     console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
     sendTextMessage(userID,data.msg);    
